32587d20b8b80cb19a5cf794ce04ffe30d769c90
kyuhyun KIM <kkh30123@naver.com>
2023-07-11 10:55:05 +0900

    Modified

--- SQLExcepsionTest/src/com/kimmingyu0/springframe/dao/DuplicateUserIdExceotion.java
+++
@@ -1,7 +0,0 @@
-package com.kimmingyu0.springframe.dao;
-
-public class DuplicateUserIdExceotion extends RuntimeException {
-    public DuplicateUserIdExceotion (Throwable cause){
-        super(cause);
-    }
-}
