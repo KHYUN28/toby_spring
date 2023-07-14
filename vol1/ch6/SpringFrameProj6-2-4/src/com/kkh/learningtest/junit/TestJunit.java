a13d621e30e28b79a107fc4fe6221274b89a7ac1
kyuhyun KIM <kkh30123@naver.com>
2023-07-14 09:14:47 +0900

    Modified

--- vol1/ch6/SpringFrameProj6-2-4/src/com/kkh/learningtest/junit/TestJunit.java
+++
@@ -1,8 +0,0 @@
-package com.kkh.learningtest.junit;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class TestJunit {
-
-}
--- vol1/ch6/SpringFrameProj6-3-4/src/com/kkh/springframe/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.kkh.springframe.service;
-
-import com.kkh.springframe.domain.User;
-
-public interface UserService {
-	void add(User user); // 단위 테스트 add()
-	void upgradeLevels();
-}
-
